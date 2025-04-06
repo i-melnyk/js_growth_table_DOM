a750d93786fdea3569cd7ad173937b9aaf10d26e
Ihor <i-melnyk@github.io>
2025-04-06 16:42:33 +0300

    added refs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const refs = {
+  appendRow: document.querySelector('.append-row'),
+  removeRow: document.querySelector('.remove-row'),
+  appendColumn: document.querySelector('.append-column'),
+  removeColumn: document.querySelector('.append-column'),
+};
+
+refs.appendRow.addEventListener('click', (e) => {
+//   console.log('append-row');
+});
+
+refs.removeRow.addEventListener('click', (e) => {
+//   console.log('remove-row');
+});
+
+refs.appendColumn.addEventListener('click', (e) => {
+//   console.log('append-column');
+});
+
+refs.removeColumn.addEventListener('click', (e) => {
+//   console.log('append-column');
+});
